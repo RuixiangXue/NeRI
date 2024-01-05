b9d53ecda31774348e7849e071ef63b118e4ea54
RuiXX <85331222+RuixiangXue@users.noreply.github.com>
2024-01-05 11:58:21 +0800

    Update train.py

--- train.py
+++ train.py
@@ -61,7 +61,6 @@ def main():
     parser.add_argument('--quant_axis', type=int, default=0, help='quantization axis (-1 means per tensor)')
     parser.add_argument('--eval_fps', action='store_true', default=False, help='fwd multiple times to test the fps ')
     parser.add_argument('--dump', action='store_true', default=False, help='dump the prediction images')
-    parser.add_argument('--segmentation', action='store_true', default=False, help='segmentation or not')
     # pruning paramaters
     parser.add_argument('--prune_steps', type=float, nargs='+', default=[0.,], help='prune steps')
     parser.add_argument('--prune_ratio', type=float, default=1.0, help='pruning ratio')
@@ -497,14 +496,11 @@ def evaluate(model, val_dataloader, Temporal_PE,Rotation_PE,Translation_PE, loca
             write_ri(seg_ri_filename, pc_seg)
         
         output_pc_rec = range2pc(output_list[0].squeeze(0),args.cfg) #(64,2000,3)
-        if args.segmentation:
-            pc_seg = (pc_seg.permute(1,2,0)).numpy() 
-            output_pc_rec = (output_pc_rec * pc_seg).reshape(-1,3) # (64*2000, 3)
-            output_pc_rec = np.unique(output_pc_rec,axis=0)
 
-        else:
-            output_pc_rec = output_pc_rec.reshape(-1,3) # (64*2000, 3)
-            output_pc_rec = np.unique(output_pc_rec,axis=0)
+        pc_seg = (pc_seg.permute(1,2,0)).numpy() 
+        output_pc_rec = (output_pc_rec * pc_seg).reshape(-1,3) # (64*2000, 3)
+        output_pc_rec = np.unique(output_pc_rec,axis=0)
+
         pc = pc.reshape(-1,3).numpy()
         pc = np.unique(pc,axis=0)
         
